9eced8c38a75c09291db184d038415f43a3f57e2
Alexander Templeton <azide0x37@users.noreply.github.com>
2015-07-12 19:52:30 -0500

    Update wifey.py

--- wifey.py
+++ wifey.py
@@ -1,1 +1,17 @@
+'''
+wifey.py
+Class implementation of wifey module
+'''
 
+class Wifey(Object):
+    def __init__(self, accounts = [], username = '', password = '', pin = '0000'):
+
+    def __str__(self):
+
+    def __str__(self):
+
+    def __str__(self):
+
+    def __str__(self):
+
+    def __str__(self):

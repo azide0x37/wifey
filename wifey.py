768742882aaf206532d522660d3841ca362fb1cf
Alexander Templeton <blakmatr@gmail.com>
2015-08-04 05:04:52 -0500

    kay..

--- wifey.py
+++ wifey.py
@@ -6,7 +6,12 @@ Class implementation of wifey module
 class Wifey(Object):
     def __init__(self, accounts = [], username = '', password = '', pin = '0000'):
         self.accounts = accounts
+        self.username = username
+        self.password = password
         
+        if pin != '0000':
+            self.pin = pin
+
     def __str__(self):
         try:
             return self.accounts[0]

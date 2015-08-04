7a63540b515a5511966fb60649ca98839fe9e60c
Alexander Templeton <blakmatr@gmail.com>
2015-08-04 05:02:20 -0500

    getting vimflow down Please enter the commit message for your changes. Lines starting

--- wifey.py
+++ wifey.py
@@ -5,7 +5,8 @@ Class implementation of wifey module
 
 class Wifey(Object):
     def __init__(self, accounts = [], username = '', password = '', pin = '0000'):
-
+        self.accounts = accounts
+        
     def __str__(self):
         try:
             return self.accounts[0]

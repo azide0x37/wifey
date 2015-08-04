0a0efb0384de8687c9ee8771e2109938410d1f82
Alexander Templeton <blakmatr@gmail.com>
2015-08-04 03:51:32 -0500

    added str method

--- wifey.py
+++ wifey.py
@@ -7,11 +7,4 @@ class Wifey(Object):
     def __init__(self, accounts = [], username = '', password = '', pin = '0000'):
 
     def __str__(self):
-
-    def __str__(self):
-
-    def __str__(self):
-
-    def __str__(self):
-
-    def __str__(self):
+        return self.accounts[0]
